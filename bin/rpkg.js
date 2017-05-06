caa37e0c858896a03a4027eaeb6320079208c9aa
manoloedge96@gmail.com <manoloedge96@gmail.com>
2017-05-06 21:54:54 +0200

    added logs to rpkg

--- bin/rpkg.js
+++ bin/rpkg.js
@@ -1,18 +1,31 @@
 #!/usr/bin/env node
 'use strict';
-const fs = require('fs');
-const CWD = process.cwd();
-var JSZip = require("jszip")();
+const fs   = require('fs');
+const clc  = require('cli-color');
+const CWD  = process.cwd();
+const ARGS = process.argv.slice(2);
+
+/* TODO: add flags, one for hiding logs */
+let log     = process.stdout;
+let lgray   = clc.xterm(59).bold;
+let info    = clc.xterm(23);
+let red     = clc.xterm(9);
+let warning = clc.xterm(3);
+let success = clc.xterm(28);
+let errorcl = clc.xterm(1);
+let JSZip   = require("jszip")();
 
 function addFile(path) {
+  log.write('  Processing: '+lgray(path)+'\n');
   JSZip.file(path, fs.readFileSync(CWD+'/'+path));
 }
 function addFolder(path) {
+  log.write('  Processing: '+info(path)+'\n');
   JSZip.folder(path);
-  let files = fs.readdirSync(CWD+'\\'+path);
+  let files = fs.readdirSync(CWD+'/'+path);
   files.forEach(file => {
-    if (fs.lstatSync(path+'\\'+file).isDirectory()) {
-      addFolder(path+'\\'+file);
+    if (fs.lstatSync(path+'/'+file).isDirectory()) {
+      addFolder(path+'/'+file);
     }
     else {
       addFile(path+'/'+file);
@@ -20,13 +33,24 @@ function addFolder(path) {
   })
 }
 
-if(!fs.existsSync(CWD+'/out')) fs.mkdirSync('out');
+let manifestPath = `${CWD}/config/manifest.json`;
+let config;
+if (fs.existsSync(manifestPath)) {
+  config = require(manifestPath);
+}
+
+log.write(`Started packaging development ${config ? '['+success(config.name)+']': ''}...\n`);
+
+if(!fs.existsSync(CWD+'/out')) {
+  console.log(`[${info('info')}] Created ${info('out')} folder.\n`);
+  fs.mkdirSync('out');
+}
 addFolder('src');
 addFolder('config');
-addFile('package.json')
+addFile('package.json');
 
 JSZip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
   .pipe(fs.createWriteStream(CWD+'/out/app.tsk'))
   .on('finish', function () {
-    console.log("Development packed to 'out/app.tsk'");
+    console.log("\n["+success('ok')+"] Development packed to "+info('out/app.tsk'));
   });
