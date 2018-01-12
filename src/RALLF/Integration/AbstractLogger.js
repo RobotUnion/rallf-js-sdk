b65924ba6ca0a909d5590085d10000803e40952d
manoloedge96@gmail.com <manoloedge96@gmail.com>
2018-01-12 16:59:34 +0100

    refactored loggers

---
+++ src/RALLF/Integration/AbstractLogger.js
@@ -0,0 +1,123 @@
+class AbstractLogger {
+  constructor(notifier) {
+    this.notifier = notifier;
+    this.LOG_SEVERITY_EMERGENCY = 0;
+    this.LOG_SEVERITY_ALERT = 1;
+    this.LOG_SEVERITY_CRITICAL = 2;
+    this.LOG_SEVERITY_ERROR = 3;
+    this.LOG_SEVERITY_WARNING = 4;
+    this.LOG_SEVERITY_NOTICE = 5;
+    this.LOG_SEVERITY_INFO = 6;
+    this.LOG_SEVERITY_DEBUG = 7;
+  }
+
+  /**
+   * @param string message
+   * @param mixed data
+   * @param int severity , standard RFC3164 code (https://tools.ietf.org/html/rfc3164)
+   * @param string channel
+   */
+  log(message, data = null, severity = 7, channel = "") {
+    let log =
+      {
+        'time': Date.now(),
+        'severity': severity,
+        'channel': channel,
+        'message': message,
+        'data': data
+      }
+    this.notifier.notify(log)
+  }
+
+  capture(device, severity = 7, channel = "") {
+    return new Promise((resolve, reject) => {
+      device.takeScreenshot((err, resp) => {
+        if (err) {
+          this.error(
+            'error',
+            { "error": err },
+            null,
+            severity,
+            channel
+          );
+          reject({ "error": err })
+        }
+        else {
+          this.debug(
+            'capture',
+            { "capture": resp },
+            null,
+            severity,
+            channel
+          );
+          resolve({ "capture": resp })
+        }
+      })
+    })
+  }
+
+  /**
+   * @param $message
+   * @param null $data
+   * @param string $channel
+   */
+  debug(message, data = null, channel = "") {
+    this.log(message, data, this.LOG_SEVERITY_DEBUG, channel);
+  }
+
+  /**
+   * @param $message
+   * @param null $data
+   * @param string $channel
+   */
+  warning(message, data = null, channel = "") {
+    this.log(message, data, this.LOG_SEVERITY_WARNING, channel);
+  }
+
+  /**
+   * @param $message
+   * @param null $data
+   * @param string $channel
+   */
+  alert(message, data = null, channel = "") {
+    this.log(message, data, this.LOG_SEVERITY_ALERT, channel);
+  }
+
+  /**
+   * @param $message
+   * @param null $data
+   * @param string $channel
+   */
+  emergency(message, data = null, channel = "") {
+    this.log(message, data, this.LOG_SEVERITY_EMERGENCY, channel);
+  }
+
+  /**
+   * @param $message
+   * @param null $data
+   * @param string $channel
+   */
+  critical(message, data = null, channel = "") {
+    this.log(message, data, this.LOG_SEVERITY_CRITICAL, channel);
+  }
+
+  /**
+   * @param $message
+   * @param null $data
+   * @param string $channel
+   */
+  error(message, data = null, channel = "") {
+    this.log(message, data, this.LOG_SEVERITY_ERROR, channel);
+  }
+
+  /**
+   * @param $message
+   * @param null $data
+   * @param string $channel
+   */
+  info(message, data = null, channel = "") {
+    this.log(message, data, this.LOG_SEVERITY_INFO, channel);
+  }
+}
+
+module.exports = AbstractLogger;
--- src/RALLF/Integration/ConsoleLogger.js
+++ src/RALLF/Integration/ConsoleLogger.js
@@ -1,105 +1,40 @@
-class ConsoleLogger {
+const AbstractLogger = require('./AbstractLogger')
+class ConsoleLogger extends AbstractLogger {
   constructor() {
-    this.LOG_SEVERITY_EMERGENCY = 0;
-    this.LOG_SEVERITY_ALERT = 1;
-    this.LOG_SEVERITY_CRITICAL = 2;
-    this.LOG_SEVERITY_ERROR = 3;
-    this.LOG_SEVERITY_WARNING = 4;
-    this.LOG_SEVERITY_NOTICE = 5;
-    this.LOG_SEVERITY_INFO = 6;
-    this.LOG_SEVERITY_DEBUG = 7;
-  }
-
-  /**
-   * @param string message
-   * @param mixed data
-   * @param int severity , standard RFC3164 code (https://tools.ietf.org/html/rfc3164)
-   * @param string channel
-   */
-  log(message, data = null, severity = 7, channel = "") {
-    let log =
-      {
-        'time': Date.now(),
-        'severity': severity,
-        'channel': channel,
-        'message': message,
-        'data': data
+    super({
+      notify: log => {
+        console.log(` [${(new Date(log.time)).toLocaleString()}] - ${log.message}`)
       }
-    console.log(` [${(new Date(log.time)).toLocaleString()}] - ${log.message}`)
-  }
-
-  capture(device, saveLocal, cback) {
-    let captureFN = saveLocal ? device.saveScreenshot.bind(device, 'img_' + Date.now() + '.png') :
-      device.takeScreenshot
-    captureFN((err, res) => {
-      this.debug('Capture', {
-        "capture": res
-      }, null, this.LOG_SEVERITY_DEBUG, "");
-      if (typeof cback === 'function') cback(res);
     })
   }
+  capture(device, saveLocal, cback) {
+    return new Promise((resolve, reject) => {
+      let captureFN = saveLocal ? device.saveScreenshot.bind(device, 'img_' + Date.now() + '.png') :
+        device.takeScreenshot;
 
-  /**
-   * @param $message
-   * @param null $data
-   * @param string $channel
-   */
-  debug(message, data = null, channel = "") {
-    this.log(message, data, this.LOG_SEVERITY_DEBUG, channel);
-  }
-
-  /**
-   * @param $message
-   * @param null $data
-   * @param string $channel
-   */
-  warning(message, data = null, channel = "") {
-    this.log(message, data, this.LOG_SEVERITY_WARNING, channel);
-  }
-
-  /**
-   * @param $message
-   * @param null $data
-   * @param string $channel
-   */
-  alert(message, data = null, channel = "") {
-    this.log(message, data, this.LOG_SEVERITY_ALERT, channel);
-  }
-
-  /**
-   * @param $message
-   * @param null $data
-   * @param string $channel
-   */
-  emergency(message, data = null, channel = "") {
-    this.log(message, data, this.LOG_SEVERITY_EMERGENCY, channel);
-  }
-
-  /**
-   * @param $message
-   * @param null $data
-   * @param string $channel
-   */
-  critical(message, data = null, channel = "") {
-    this.log(message, data, this.LOG_SEVERITY_CRITICAL, channel);
-  }
-
-  /**
-   * @param $message
-   * @param null $data
-   * @param string $channel
-   */
-  error(message, data = null, channel = "") {
-    this.log(message, data, this.LOG_SEVERITY_ERROR, channel);
-  }
-
-  /**
-   * @param $message
-   * @param null $data
-   * @param string $channel
-   */
-  info(message, data = null, channel = "") {
-    this.log(message, data, this.LOG_SEVERITY_INFO, channel);
+      captureFN((err, resp) => {
+        if (err) {
+          this.error(
+            'error',
+            { "error": err },
+            null,
+            this.LOG_SEVERITY_DEBUG,
+            ""
+          );
+          reject({ "error": err })
+        }
+        else {
+          this.debug(
+            'capture',
+            { "capture": resp },
+            null,
+            this.LOG_SEVERITY_DEBUG,
+            ""
+          );
+          resolve({ "capture": resp })
+        }
+      })
+    })
   }
 }
 
