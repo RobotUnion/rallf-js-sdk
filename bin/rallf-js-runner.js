195e3d9fcbe5ee9e68e0b4c08f0a2d4f4cec09fa
Mona Lisa <manoloedge96@gmail.com>
2018-10-02 13:04:18 +0200

    fixed runner

--- bin/rallf-js-runner.js
+++ bin/rallf-js-runner.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings
 
 const argv = require('yargs').argv;
 const path = require('path');
@@ -6,74 +6,113 @@ const fs = require('fs');
 const wdClient = require('wd');
 const IdeLogger = require('../src/Integration/IdeLogger');
 
-console.log("Args", argv);
+// console.log("Args", argv);
 
-
-if (!argv.task_path) {
-  argv.task_path = process.cwd();
-}
-
-if (!argv.manifest_path) {
-  argv.manifest_path = path.join(process.cwd(), 'config', 'manifest.json');
-}
-
-if (!argv.task_path || !argv.manifest_path) {
-  console.log('  ERROR: Args are required.');
-  console.log(`  Usage: ${argv['$0']} --task_path=<path> --manifest_path=<path>`);
-  process.stderr.write('Finished: error');
-  return 1;
-}
-
-
-let task_path = path.resolve(argv.task_path);
-let manifest_path = path.resolve(argv.manifest_path);
-let manifest = JSON.parse(fs.readFileSync(manifest_path).toString());
-const robot = JSON.parse(argv.robot || '{}');
-const input = JSON.parse(argv.input || '{}');
+const task_path = argv.task_path || '.';
+const robot = argv.robot || '{}';
+const input = argv.input || '{}';
+const manifest_path = path.resolve(task_path) + '/config/manifest.json';
+const manifest = JSON.parse(fs.readFileSync(manifest_path).toString());
 const mainFile = manifest.main;
 const capabilities = manifest.capabilities;
-const driver = wdClient.remote('localhost', 4444);
+const type = manifest.type;
+
+let taskLogger;
+taskLogger = new IdeLogger(process);
+taskLogger.info('Setting up');
+
+
+const runner = {
+  driver: null,
+  task: null,
+  finish(status_code = 0) {
+    if (this.driver) {
+      this.driver.quit(() => {
+        process.stdout.write('finished: with asd code ' + status_code);
+        process.exit(status_code);
+      });
+    }
+    else {
+      process.stdout.write('finished: with code ' + status_code);
+      return process.exit(status_code);
+    }
+  },
+  isWeb(task) {
+    return task.type.includes('web');
+  },
+  isAndroid(task) {
+    return task.type.includes('android');
+  },
+  isStandalone(task) {
+    return task.type.includes('standalone');
+  },
+  getRobot() {
+    taskLogger.debug('robot: ' + robot);
+
+    return {
+      self: {},
+      kb: {},
+      ...robot
+    };
+  },
+  getInput() {
+
+  },
+  run() {
+    try {
+      this.task.setDevice(this.driver);
+      this.task.setRobot(runner.getRobot());
+      this.task.setInput(runner.getInput());
+      let self = this;
+      this.task.onFinish = (status_code) => {
+        self.finish(status_code);
+      };
+      taskLogger.debug('running');
+      this.task.run();
+    } catch (error) {
+      process.stderr.write('error: ' + error);
+      this.finish(1);
+    }
+  }
+};
+
+// runner.finish(0);
+
+// resolve task path
 const taskPath = path.resolve(task_path + '/' + mainFile);
+
+// Remove from cache
 delete require.cache[taskPath];
 
+// Import task
+const Task = require(taskPath);
 
-let Task;
-try {
-  Task = require(path.join(task_path, mainFile));
+// Create task instance
+let task = new Task();
 
-  let taskLogger;
-  taskLogger = new IdeLogger(process);
-  taskLogger.info('Setting up');
+task.setLogger(taskLogger);
 
-  let task = new Task();
-  task.name = manifest.name;
+task.name = manifest.name;
+task.type = manifest.type;
+task.version = manifest.version;
 
-  driver.init(capabilities, (err, sess) => {
+runner.task = task;
+
+// If its not standalone we need to launch webdriver
+if (!runner.isStandalone(task)) {
+  runner.driver = wdClient.remote('localhost', 4444);
+  runner.driver.init(capabilities, (err, sess) => {
+    taskLogger.debug('init: ' + sess);
+
     if (err) {
-      driver.quit();
+      runner.driver.quit();
       process.stderr.write('error: ' + err);
       return process.exit(1);
     }
-    task.setDevice(driver);
-    task.setLogger(taskLogger);
-    task.setRobot(JSON.parse(robot));
-    task.setInput(JSON.parse(input));
-    task.onFinish = function (x) {
-      driver.quit(() => {
-        process.stdout.write('finished: ' + JSON.stringify(x));
-        process.exit(0);
-      });
-    };
-    task.run();
+
+    runner.run();
   });
-} catch (error) {
-  driver.quit(() => {
-    if (error.toString().includes('ECONNREFUSED')) {
-      process.stderr.write('\nerror: Seem like you cant connect, please try again or contact us');
-    }
-    else if (!error.includes('DeprecationWarning:')) {
-      process.stderr.write('\nerror: ' + JSON.stringify(error));
-    }
-    process.exit(1);
-  });
+} else {
+  runner.run();
 }
+
