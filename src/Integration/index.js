41913bc4c90bb407e92977de54baacab3bcb3f27
Mona Lisa <manoloedge96@gmail.com>
2018-10-17 16:33:33 +0200

    fixed weird files

--- src/Integration/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  Task: require('./Task'),
-  RallfLogger: require('./RallfLogger'),
-  IdeLogger: require('./IdeLogger'),
-  ConsoleLogger: require('./ConsoleLogger'),
-  AbstractLogger: require('./AbstractLogger')
-}
