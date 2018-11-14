0fa6f126479c4127f01a94dcdde67ba7e01acc77
Mona Lisa <manoloedge96@gmail.com>
2018-11-14 12:12:15 +0100

    fixed timing response was wrong

--- examples/robot-example/src/main.js
+++ examples/robot-example/src/main.js
@@ -8,29 +8,24 @@ class BasicExample extends Task {
 
   async warmup() {
     this.logger.debug('warmup');
-    this.firefox = await this.devices.get('firefox');
-
-    this.on('event:external', (data) => {
-      this.logger.debug('event:external', data);
-    });
   }
 
   async start(input) {
     this.logger.debug('BasicExample started');
 
-    // await this.firefox.get('https://github.com');
-    this.robot.ensureFile('data.json');
-    this.robot.saveJSON('data.json', { test: 'asdads' });
+    // // await this.firefox.get('https://github.com');
+    // this.robot.ensureFile('data.json');
+    // this.robot.saveJSON('data.json', { test: 'asdads' });
 
-    this.robot.ensureFile('data.txt');
-    this.robot.saveFile('data.txt', 'This is a test text file', {});
+    // this.robot.ensureFile('data.txt');
+    // this.robot.saveFile('data.txt', 'This is a test text file', {});
 
     
-    // Or you can do
-    this.robot.createFile('data.json', { test: 'asdads' });
+    // // Or you can do
+    // this.robot.createFile('data.json', { test: 'asdads' });
 
 
-    this.logger.debug('Data saved: ', data);
+    // this.logger.debug('Data saved: ', data);
     return 'finished';
   }
 
