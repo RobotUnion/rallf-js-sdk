709f5611f3a93cc0197c229cb41629f7b113b13c
nombrekeff <manoloedge96@gmail.com>
2020-02-13 12:38:07 +0100

    feat: made std notifier until implemented in LogginJS itself, to be able to output error through stderr

--- src/lib/logging.js
+++ src/lib/logging.js
@@ -1,11 +1,36 @@
-'use strict';
-const loggin = require('loggin-js');
-const pkg = require('../../package.json');
+"use strict";
+const loggin = require("loggin-js");
+const pkg = require("../../package.json");
 
-const logger = loggin.logger('console', {
+class StdNotifier extends loggin.Notifier {
+  constructor(options) {
+    super(options, "std");
+    this.lineIndex = 0;
+  }
+
+  output(message, log) {
+    let logOut = message;
+    if (this.options.lineNumbers) {
+      logOut = this.getLineWithNumber(message);
+    }
+
+    if (log.level.level <= loggin.severity("ERROR").level) {
+      process.stderr.write(logOut + "\n");
+    } else {
+      process.stdout.write(logOut + "\n");
+    }
+
+    return this;
+  }
+}
+
+const logger = loggin.logger({
   channel: pkg.name,
-  formatter: process.env.LOGGER_FORMATTER || 'detailed',
-  level: loggin.severity(process.env.DEBUG ? 'DEBUG' : 'INFO'),
+  formatter: process.env.LOGGER_FORMATTER || "detailed",
+  level: loggin.severity(process.env.DEBUG ? "DEBUG" : "INFO"),
+  notifiers: [
+    new StdNotifier()
+  ]
 });
 
 module.exports.loggin = loggin;
